4c06f486c19ee05900f153ce6e149486c566dcf3
daasno <92796341+daasno@users.noreply.github.com>
2022-06-09 10:43:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import math as mt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.preprocessing import OneHotEncoder
+import plotly.express as px
 
 st.set_page_config(
     page_title="My Cig Quantities Prediction",
@@ -23,28 +25,17 @@ Upload the order Csv file for predictions.
 )
 
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+# ab_default = None
+# result_default = None
 
 
-ab_default = None
-result_default = None
-
-if uploaded_file:
-    df = pd.read_csv("new_geek.csv")
-    # The new_df got no quantities variable
-    new_df = pd.read_csv(uploaded_file)
-    ##########Data Preprocessing##########
-    reg = GradientBoostingRegressor(n_estimators=250, random_state=42,
-                                    max_features='auto')
-    ohe = OneHotEncoder()
+def preprocessing(new_df):
     encoded_cat = ohe.fit_transform(new_df[['Catégorie']]).toarray()
     encoded_cat_labels = np.array(ohe.categories_).ravel()
     cat_ohe = pd.DataFrame(encoded_cat, columns=encoded_cat_labels)
     new_geek = pd.concat([new_df, cat_ohe], axis=1)
-    cat = new_geek['Catégorie']
     new_geek.drop('Catégorie', inplace=True, axis=1)
     new_geek.drop('Marque', inplace=True, axis=1)
-    names = new_geek['Produit']
-    decl = new_geek['Déclinaison']
     new_geek.drop('Produit', inplace=True, axis=1)
     new_geek.drop('Fournisseur', inplace=True, axis=1)
     new_geek['Déclinaison'] = new_geek['Déclinaison'].fillna("None")
@@ -64,19 +55,35 @@ if uploaded_file:
                             new_geek['Grey-Gris fonce'] + new_geek['Violet-Neon-Chestnut-Sky M'] + new_geek['0,6ohms'] + \
                             new_geek['065'] + new_geek['03'] + new_geek['Chrome'] + new_geek['None'] + new_geek['Noir']
     new_geek.drop(
-        ['05', '0,7ohms', '0,25ohms', '1,4ohms', 'Pods', 'Orange', 'Rose', 'Vert', 'Rainbow', 'Bleu', 'Scarlet-Rouge', '0,4ohms',
-         'Gold', '04', 'Grey-Gris fonce', 'Violet-Neon-Chestnut-Sky M', '0,6ohms', '065', '03', 'Chrome', 'None',
-         'Noir'], inplace=True,axis=1)
+        ['05', '0,7ohms', '0,25ohms', '1,4ohms', 'Pods', 'Orange', 'Rose', 'Vert', 'Rainbow', 'Bleu', 'Scarlet-Rouge',
+         '0,4ohms', 'Gold', '04', 'Grey-Gris fonce', 'Violet-Neon-Chestnut-Sky M', '0,6ohms', '065', '03', 'Chrome',
+         'None', 'Noir'], inplace=True, axis=1)
     new_geek['other_cat'] = new_geek['Cartouches vides pour pods'] + new_geek['Mods'] + new_geek['RBA'] + new_geek[
         'Drip tips'] + new_geek['Adaptateurs 510'] + new_geek['Pyrex'] + new_geek['Autres'] + new_geek['RTA'] + \
                             new_geek['Clearos'] + new_geek['Kits'] + new_geek['Fil Résistif'] + new_geek[
                                 'RDA (Drippers)'] + new_geek['Mods electroniques']
     new_geek.drop(
         ['Cartouches vides pour pods', 'Mods', 'RBA', 'Drip tips', 'Adaptateurs 510', 'Pyrex', 'Autres', 'RTA',
-         'Clearos', 'Kits', 'Fil Résistif', 'RDA (Drippers)', 'Mods electroniques'], inplace=True,axis=1)
-    # ###########Data preview##############
-    # st.markdown("### Data preview")
-    # st.dataframe(new_geek.head(10))
+         'Clearos', 'Kits', 'Fil Résistif', 'RDA (Drippers)', 'Mods electroniques'], inplace=True, axis=1)
+    return new_geek
+
+
+if uploaded_file:
+    df = pd.read_csv("new_geek.csv")
+    # The new_df got no quantities variable
+    new_df = pd.read_csv(uploaded_file)
+    names = new_df['Produit']
+    decl = new_df['Déclinaison']
+    cat = new_df['Catégorie']
+    ##########Data Preprocessing##########
+    reg = GradientBoostingRegressor(n_estimators=250, random_state=42,
+                                    max_features='auto')
+    ############Data preview##############
+    st.markdown("### Data preview")
+    st.dataframe(new_df.head(10))
+
+    ohe = OneHotEncoder()
+    new_geek = preprocessing(new_df)
 
     st.markdown("### Predict")
     #st.dataframe(new_geek.head(10))
@@ -89,3 +96,5 @@ if uploaded_file:
         pred = np.ceil(pred)
         res = pd.concat([names, cat, decl, pd.DataFrame(pred, columns=['Predicted'])], axis=1)
         st.write(res)
+        fig = px.pie(res, values='Predicted', names='Catégorie', title='Product categories')
+        fig.show()
